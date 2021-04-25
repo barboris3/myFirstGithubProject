26ae52d898fda38e7c5e39fe42874fda7d3e74a4
unknown <hegigiy413>
2021-04-25 13:43:14 +0300

    script add dark green font

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ setTimeout(myFunc, 2000);
 function myFunc() {
 	let el = document.createElement('p');
 	el.innerHTML = 'This is my first Github project';
+	el.style.color = '#2F4F4F';
 	document.body.append(el);
 }
